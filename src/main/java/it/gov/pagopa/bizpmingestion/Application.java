acbf73dcdaec2a41d423c39697c090d767800d5e
Jacopo Carlini <jacopo.carlini@pagopa.it>
2024-12-05 11:47:35 +0100

    set default timezone

--- src/main/java/it/gov/pagopa/bizpmingestion/Application.java
+++ src/main/java/it/gov/pagopa/bizpmingestion/Application.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Application {
 
@@ -12,6 +15,10 @@ public class Application {
   private String threadsNumber;
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Europe/Rome")));
+
+    System.out.println(TimeZone.getDefault());
+
     SpringApplication.run(Application.class, args);
   }
 
