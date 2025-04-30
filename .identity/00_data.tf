dbc5a3d5a8eda38a4266f7c75567060daad31800
svariant <samuele.varianti@nttdata.com>
2025-04-30 16:42:02 +0200

    chore: [PIDM-488] Implement workload identity

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -3,11 +3,6 @@ data "azurerm_storage_account" "tfstate_app" {
   resource_group_name = "io-infra-rg"
 }
 
-data "azurerm_user_assigned_identity" "identity_cd" {
-  resource_group_name = "${local.product}-identity-rg"
-  name                = "${local.product}-${local.domain}-01-github-cd-identity"
-}
-
 data "azurerm_resource_group" "dashboards" {
   name = "dashboards"
 }
@@ -64,4 +59,14 @@ data "azurerm_cosmosdb_account" "bizevents_cosmos" {
 data "azurerm_key_vault_secret" "key_vault_integration_test_webhook_slack" {
   name         = "webhook-slack"
   key_vault_id = data.azurerm_key_vault.domain_key_vault.id
+}
+
+data "azurerm_user_assigned_identity" "workload_identity_clientid" {
+  name                = "bizevents-workload-identity"
+  resource_group_name = "pagopa-${var.env_short}-${local.location_short}-${var.env}-aks-rg"
+}
+
+data "azurerm_user_assigned_identity" "identity_cd_01" {
+  resource_group_name = "${local.product}-identity-rg"
+  name                = "${local.product}-${local.domain}-job-01-github-cd-identity"
 }
